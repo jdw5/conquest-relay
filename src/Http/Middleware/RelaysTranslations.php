d9372d817116880ece13ae19257652dbf4b59b31
jdw5 <joshuadwallace1@gmail.com>
2024-07-07 00:35:43 +1000

    Docstring for share

--- src/Http/Middleware/RelaysTranslations.php
+++ src/Http/Middleware/RelaysTranslations.php
@@ -8,6 +8,13 @@ use Inertia\Middleware;
 
 class RelaysTranslations extends Middleware
 {
+    /**
+     * Defines the props that are shared by default.
+     *
+     * @see https://inertiajs.com/shared-data
+     *
+     * @return array
+     */
     public function share(Request $request): array
     {
         app()->setLocale(Relay::getLanguage());
